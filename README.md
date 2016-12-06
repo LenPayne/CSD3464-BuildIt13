545ddaf31d614f0797ae8ba631c35449efa85a5c
Len Payne <len.payne@lambtoncollege.ca>
2016-12-05 21:38:12 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# CSD-3464 Build It Samples for Lesson #13
+## Database Connections
+
+This repo contains samples for a lesson based on Database Connections.
+
+To retrieve these files and work with them:
+
+1. Open NetBeans
+2. Navigate to Team -> Git -> Clone... or Team -> Git -> Remote -> Clone...
+3. Enter the Repository URL: https://github.com/LenPayne/CSD3464-BuildIt13.git
+4. Ensure the *Clone Into:* folder is on the F: Drive
+5. Hit Finish, Work from There
+
+## Included In This File
+
+1. Performing a Static Statement and Outputting Results
+2. Performing a Dynamic Statement and Outputting Results
+3. Performing an End-to-End set of CRUD Operations
+
+Note: The default version ONLY works on-campus. Attached is an SQL file to setup the Sample DB in MySQL.
---
+++ sample.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `sample` (
+  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `name` varchar(255) default NULL,
+  `age` mediumint default NULL,
+  PRIMARY KEY (`id`)
+) AUTO_INCREMENT=1;
+
+INSERT INTO `sample` (`name`,`age`) VALUES ("Wyatt Walls",36),("Katell Calderon",35),("Anjolie Wiley",49),("Colette Gray",39),("Nina Ramsey",27),
+("Stewart Nielsen",57),("Linda Mendez",68),("Zena Franco",59),("Kennan Tran",51),("Emma Tillman",59),("Brenden Lucas",50),("Melanie Mathis",56),
+("Reece Knight",60),("Arden Lancaster",48),("Gary Mcdowell",55),("Molly Rasmussen",67),("Eleanor Robinson",67),("Chancellor Osborn",37),("Tiger Cobb",17),
+("Stewart Benson",56),("Eliana Conway",18),("Kevyn Ortiz",59),("Kameko Brock",65),("Rudyard Norris",67),("Adara Bolton",30),("Hillary Garza",37),
+("Madeline Lara",30),("Dara Bullock",18),("Amity Wilson",22),("Rafael Branch",32),("Adara Cote",29),("Jin Gutierrez",57),("Karyn Calderon",21),
+("Sean Norton",39),("Louis Washington",63),("Mary Benson",28),("Blair Stein",45),("Emma Bullock",65),("Clark Rich",46),("Reed Berg",58),("Ivana Brady",21),
+("Geoffrey Gregory",35),("Courtney Mcconnell",51),("Tanner Valentine",53),("Zane Reynolds",56),("Colleen Francis",41),("Wang Wiggins",46),
+("Madeline Riddle",21),("Brynn Miller",28),("Cassady Mcintosh",55),("David Hatfield",50),("Micah Roy",19),("Thor Snider",41),("Kathleen Carney",20),
+("Cameron Mercer",20),("Ray Holcomb",26),("Sopoline Howard",38),("Kelly Hendrix",55),("Buckminster Matthews",22),("Donna Holloway",53),("Uma English",16),
+("Mona Stout",16),("Lucian Dalton",61),("Zachery Boyd",59),("Alfonso Hays",50),("TaShya Kane",41),("Hu Poole",32),("Christian Roman",37),
+("Cyrus Velasquez",17),("Virginia Frank",54),("Harriet Strickland",38),("Isadora Cooper",22),("Chava Skinner",61),("Kim Mack",60),("Kirk Kerr",41),
+("Echo Shaffer",18),("Alexandra Simon",33),("Owen Romero",24),("Maia Oliver",31),("Skyler Frost",23),("Karly Sullivan",18),("Hillary Weber",23),
+("Freya Smith",31),("Slade Raymond",52),("Wendy Mathews",19),("Maxwell Jacobson",48),("Alma Dominguez",59),("Mona Foster",28),("Rhoda Schroeder",24),
+("Kaye Slater",56),("Joseph Burton",29),("Daryl Robertson",44),("Hiroko Garcia",35),("Florence Mullen",43),("Alana Morgan",40),("Elvis Sears",24),
+("Michael House",28),("Cullen Joseph",30),("Dakota Obrien",45),("Laith Foley",43);
